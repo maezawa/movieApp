6936cadb9109e3062edee7d85a637aaa8c812512
Gris-Bleu <gris-bleu@Gris-Bleu-no-Mac-mini.local>
2015-02-14 15:28:02 +0900

    Initial Commit

---
+++ mv_A1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  mv_A1
+//
+//  Created by Gris-Bleu on 2015/02/14.
+//  Copyright (c) 2015年 Gris-Bleu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
