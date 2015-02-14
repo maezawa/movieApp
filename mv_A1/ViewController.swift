dc6412dce8aef4ff961220e29c7ed6821d49d5fa
Gris-Bleu <gris-bleu@Gris-Bleu-no-Mac-mini.local>
2015-02-15 01:09:45 +0900

    p

--- mv_A1/ViewController.swift
+++ mv_A1/ViewController.swift
@@ -10,11 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet var movieCollection: [UICollectionView]!
-
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationController?.navigationBarHidden = true
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
